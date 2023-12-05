ae27e83e8474be439f408bfd8e848c1b9f53dbc4
masleeh <102211370+masleeh@users.noreply.github.com>
2023-12-05 22:59:18 +0300

    fix config

--- next.config.js
+++ next.config.js
@@ -2,8 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    path: '/masleeh.com'
-  }
+    path: '/masleeh.com/'
+  },
+  basePath: '/masleeh.com'
 }
 
 module.exports = nextConfig
